17861b373fbe8f030efce882d0bd0c9c5dc0bd35
Andrei3223 <a.gruzitski@gmail.com>
2024-11-03 12:36:02 +0300

    liter v2

--- minitorch/datasets.py
+++ minitorch/datasets.py
@@ -4,7 +4,8 @@ from dataclasses import dataclass
 from typing import List, Tuple
 
 
-def make_pts(N):
+def make_pts(N: int) -> List:
+    """Make pts"""
     X = []
     for i in range(N):
         x_1 = random.random()
@@ -20,7 +21,8 @@ class Graph:
     y: List[int]
 
 
-def simple(N):
+def simple(N: int) -> Graph:
+    """Simple"""
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -29,7 +31,8 @@ def simple(N):
     return Graph(N, X, y)
 
 
-def diag(N):
+def diag(N: int) -> Graph:
+    """Diag"""
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -38,7 +41,8 @@ def diag(N):
     return Graph(N, X, y)
 
 
-def split(N):
+def split(N: int) -> Graph:
+    """Split"""
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -47,7 +51,8 @@ def split(N):
     return Graph(N, X, y)
 
 
-def xor(N):
+def xor(N: int) -> Graph:
+    """Xor"""
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -56,7 +61,8 @@ def xor(N):
     return Graph(N, X, y)
 
 
-def circle(N):
+def circle(N: int) -> Graph:
+    """Circle"""
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -66,12 +72,13 @@ def circle(N):
     return Graph(N, X, y)
 
 
-def spiral(N):
+def spiral(N: int) -> Graph:
+    """Spiral"""
 
-    def x(t):
+    def x(t: float) -> float:
         return t * math.cos(t) / 20.0
 
-    def y(t):
+    def y(t: float) -> float:
         return t * math.sin(t) / 20.0
 
     X = [
--- minitorch/operators.py
+++ minitorch/operators.py
@@ -1,6 +1,4 @@
-"""
-Collection of the core mathematical operators used throughout the code base.
-"""
+"""Collection of the core mathematical operators used throughout the code base."""
 
 import math
 from typing import Callable, Iterable
@@ -11,22 +9,22 @@ from typing import Callable, Iterable
 
 
 def mul(x: float, y: float) -> float:
-    "$f(x, y) = x * y$"
+    """$f(x, y) = x * y$"""
     return x * y
 
 
 def id(x: float) -> float:
-    "$f(x) = x$"
+    """$f(x) = x$"""
     return x
 
 
 def add(x: float, y: float) -> float:
-    "$f(x, y) = x + y$"
+    """$f(x, y) = x + y$"""
     return x + y
 
 
 def neg(x: float) -> float:
-    "$f(x) = -x$"
+    """$f(x) = -x$"""
     return -x
 
 
@@ -36,23 +34,22 @@ def lt(x: float, y: float) -> float:
 
 
 def eq(x: float, y: float) -> float:
-    "$f(x) =$ 1.0 if x is equal to y else 0.0"
+    """$f(x) =$ 1.0 if x is equal to y else 0.0"""
     return 1.0 if x == y else 0
 
 
 def max(x: float, y: float) -> float:
-    "$f(x) =$ x if x is greater than y else y"
+    """$f(x) =$ x if x is greater than y else y"""
     return x if x > y else y
 
 
 def is_close(x: float, y: float) -> float:
-    "$f(x) = |x - y| < 1e-2$"
+    """$f(x) = |x - y| < 1e-2$"""
     return 1.0 if abs(x - y) < 1e-2 else 0.0
 
 
 def sigmoid(x: float) -> float:
-    r"""
-    $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$
+    r"""$f(x) =  \frac{1.0}{(1.0 + e^{-x})}$
 
     (See https://en.wikipedia.org/wiki/Sigmoid_function )
 
@@ -68,8 +65,7 @@ def sigmoid(x: float) -> float:
 
 
 def relu(x: float) -> float:
-    """
-    $f(x) =$ x if x is greater than 0, else 0
+    """$f(x) =$ x if x is greater than 0, else 0
 
     (See https://en.wikipedia.org/wiki/Rectifier_(neural_networks) .)
     """
@@ -80,32 +76,32 @@ EPS = 1e-6
 
 
 def log(x: float) -> float:
-    "$f(x) = log(x)$"
+    """$f(x) = log(x)$"""
     return math.log(x + EPS)
 
 
 def exp(x: float) -> float:
-    "$f(x) = e^{x}$"
+    """$f(x) = e^{x}$"""
     return math.exp(x)
 
 
 def log_back(x: float, d: float) -> float:
-    r"If $f = log$ as above, compute $d \times f'(x)$"
+    r"""If $f = log$ as above, compute $d \times f'(x)$"""
     return d / (x + EPS)
 
 
 def inv(x: float) -> float:
-    "$f(x) = 1/x$"
+    """$f(x) = 1/x$"""
     return 1.0 / x
 
 
 def inv_back(x: float, d: float) -> float:
-    r"If $f(x) = 1/x$ compute $d \times f'(x)$"
+    r"""If $f(x) = 1/x$ compute $d \times f'(x)$"""
     return -1.0 * d / (x * x)
 
 
 def relu_back(x: float, d: float) -> float:
-    r"If $f = relu$ compute $d \times f'(x)$"
+    r"""If $f = relu$ compute $d \times f'(x)$"""
     return d if x > 0 else 0
 
 
@@ -115,38 +111,41 @@ def relu_back(x: float, d: float) -> float:
 
 
 def map(fn: Callable[[float], float]) -> Callable[[Iterable[float]], Iterable[float]]:
-    """
-    Higher-order map.
+    """Higher-order map.
 
     See https://en.wikipedia.org/wiki/Map_(higher-order_function)
 
     Args:
+    ----
         fn: Function from one value to one value.
 
     Returns:
+    -------
         A function that takes a list, applies `fn` to each element, and returns a
          new list
+
     """
     return lambda l: [fn(el) for el in l]
 
 
 def negList(ls: Iterable[float]) -> Iterable[float]:
-    "Use `map` and `neg` to negate each element in `ls`"
+    """Use `map` and `neg` to negate each element in `ls`"""
     return map(neg)(ls)
 
 
 def zipWith(
-    fn: Callable[[float, float], float]
+    fn: Callable[[float, float], float],
 ) -> Callable[[Iterable[float], Iterable[float]], Iterable[float]]:
-    """
-    Higher-order zipwith (or map2).
+    """Higher-order zipwith (or map2).
 
     See https://en.wikipedia.org/wiki/Map_(higher-order_function)
 
     Args:
+    ----
         fn: combine two values
 
     Returns:
+    -------
         Function that takes two equally sized lists `ls1` and `ls2`, produce a new list by
          applying fn(x, y) on each pair of elements.
 
@@ -155,27 +154,29 @@ def zipWith(
 
 
 def addLists(ls1: Iterable[float], ls2: Iterable[float]) -> Iterable[float]:
-    "Add the elements of `ls1` and `ls2` using `zipWith` and `add`"
+    """Add the elements of `ls1` and `ls2` using `zipWith` and `add`"""
     return zipWith(add)(ls1, ls2)
 
 
 def reduce(
     fn: Callable[[float, float], float], start: float
 ) -> Callable[[Iterable[float]], float]:
-    r"""
-    Higher-order reduce.
+    r"""Higher-order reduce.
 
     Args:
+    ----
         fn: combine two values
         start: start value $x_0$
 
     Returns:
+    -------
         Function that takes a list `ls` of elements
          $x_1 \ldots x_n$ and computes the reduction :math:`fn(x_3, fn(x_2,
          fn(x_1, x_0)))`
+
     """
 
-    def rec_reduce(iterable: Iterable[float]):
+    def rec_reduce(iterable: Iterable[float]) -> float:
         result = start
         for el in iterable:
             result = fn(el, result)
@@ -185,10 +186,10 @@ def reduce(
 
 
 def sum(ls: Iterable[float]) -> float:
-    "Sum up a list using `reduce` and `add`."
+    """Sum up a list using `reduce` and `add`."""
     return reduce(add, 0)(ls)
 
 
 def prod(ls: Iterable[float]) -> float:
-    "Product of a list using `reduce` and `mul`."
+    """Product of a list using `reduce` and `mul`."""
     return reduce(mul, 1)(ls)
